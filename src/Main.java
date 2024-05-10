f51a0f1e16a6bfb4d0cd90c61a3945c78d04913b
riku-shiomi <riku_shiomi@worksjapan.co.jp>
2024-05-10 21:08:16 +0900

    fix main

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        DrinkFactory coffeeFactory = new CoffeeFactory();
+        DrinkFactory juiceFactory = new JuiceFactory();
+        Container[] containers = {new Container(coffeeFactory.createDrink("BOSS無糖ブラック")), new Container(juiceFactory.createDrink("バリヤースオレンジ"))};
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (Container container : containers) {
+            System.out.println(container.getDrinkName() + "の操作");
+            // 容器を振ってみる。
+            container.shake();
+            // 容器を開けずに飲もうとする
+            container.drink();
+            // 容器を開けてから飲む
+            container.open();
+            container.drink();
         }
     }
 }
